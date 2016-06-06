d19c22c7bd704a933a15ad605762f34971906dee
Varun <varun@varuns-macbook-air.local>
2016-06-06 11:42:12 +0530

    Initial Commit

---
+++ EatnowClone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EatnowClone
+//
+//  Created by Varun on 06/06/16.
+//  Copyright © 2016 Varun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EatnowClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EatnowClone
+//
+//  Created by Varun on 06/06/16.
+//  Copyright © 2016 Varun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
